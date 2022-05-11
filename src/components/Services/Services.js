3acadef424293ab3d6ac9f08c43a4cf10710fb28
Moin Khan <nurmoin05@gmail.com>
2022-05-11 22:47:20 +0600

    services area added

---
+++ src/components/Services/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className="services py-12">
+            <div className="services__title text-center">
+                <h2 className="text-primary text-2xl font-bold uppercase">Our services</h2>
+                <h3 className="text-4xl mt-2">Services We Provide</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './../../components/Banner/Banner';
 import Info from './../../components/Info/Info';
+import Services from './../../components/Services/Services';
 
 const Home = () => {
     return (
         <div className="Home">
             <Banner />
             <Info />
+            <Services />
         </div>
     );
 };
