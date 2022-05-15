c12018e59caa7263d1c6016d8736d2e6848cc323
Moin Khan <nurmoin05@gmail.com>
2022-05-15 20:19:37 +0600

    service data fetched

--- src/components/AvailableAppointment/AvailableAppointment.js
+++ src/components/AvailableAppointment/AvailableAppointment.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 
 const AvailableAppointment = ({ date }) => {
+
+    //? service State
+    const [services, setServices] = useState([]);
+
+    //? load all services
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/doctors-portal-client-module-75/main/public/services.json')
+        .then(res => res.json())
+        .then(data => setServices(data));
+    }, [])
+    console.log(services)
+
     return (
         <div className="AvailableAppointment">
             <div className="AvailableAppointment__title">
                 <h4 className="text-center text-secondary text-2xl font-semibold">Available Appointments on {format(date, "PP")}</h4>
+            </div>
+            <div className="availableAppointment__content">
+
             </div>
         </div>
     );
