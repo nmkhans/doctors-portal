3d1f14c2774cddc4b1a04bb7c5e8072019346b80
Moin Khan <nurmoin05@gmail.com>
2022-05-18 11:05:45 +0600

    login from added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,18 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJN0sM2RXqtsn20oibC6FsUGgJ9yE4hUA",
-    authDomain: "nmk-doctors-portal.firebaseapp.com",
-    projectId: "nmk-doctors-portal",
-    storageBucket: "nmk-doctors-portal.appspot.com",
-    messagingSenderId: "570085440188",
-    appId: "1:570085440188:web:7be02d80f1675b98bfeeb9"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export {
-    auth,
-}
+export default auth;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import auth from './../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [
+        signInWithGoogle,
+        user,
+        loading,
+        error
+    ] = useSignInWithGoogle(auth);
+
+    const handleGoogleLogin = (event) => {
+        event.preventDefault();
+        signInWithGoogle();
+    }
+
     return (
-        <div className="Login">
-            <h1>This is login</h1>
+        <div className="Login flex justify-center items-center h-screen">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="card-title justify-center">Login</h2>
+                    <div class="divider">OR</div>
+                    <button onClick={handleGoogleLogin} className="btn btn-outline">Continue with google</button>
+                </div>
+            </div>
         </div>
     );
 };
