eed56ce38536e097523329d8bc62aeb43b34d82f
Moin Khan <nurmoin05@gmail.com>
2022-05-21 23:07:59 +0600

    All users data fetched

--- src/components/AllUsers/AllUsers.js
+++ src/components/AllUsers/AllUsers.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 const AllUsers = () => {
+    const { data: users, isLoading } = useQuery('users', () => (
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+    ));
+
+    if(isLoading) {
+        return <h3>Loading</h3>
+    }
+
     return (
         <div>
-            <h1>All Users</h1>
+            <h1>All Users {users.length}</h1>
         </div>
     );
 };
