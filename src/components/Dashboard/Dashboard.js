1d52f6c7ff019b52347bcdae8fcc320bb324b906
Moin Khan <nurmoin05@gmail.com>
2022-05-21 11:43:59 +0600

    data fetched on myappointment based on user

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,7 +12,7 @@ const Dashboard = () => {
                     <Outlet />
                 </div>
                 <div className="drawer-side">
-                    <label for="dashboard-sidebar" className="drawer-overlay"></label>
+                    <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                         <li><LinkTo to="/dashboard">MyAppointents</LinkTo></li>
                         <li><LinkTo to="/dashboard/account">My Account</LinkTo></li>
--- src/components/MyAppointments/MyAppointments.js
+++ src/components/MyAppointments/MyAppointments.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init';
 
 const MyAppointments = () => {
+
+    const [user] = useAuthState(auth);
+    const { data: appointments, isLoading, refetch } = useQuery(['user-booking', user], () => (
+        fetch(`http://localhost:5000/booking?email=${user.email}`)
+            .then(res => res.json())
+    ))
+
+    if (isLoading) {
+        return <h3>Loading</h3>
+    }
+
     return (
         <div>
             <h3>This is my appointment</h3>
